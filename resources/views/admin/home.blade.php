503bbd3465e8ccae76d4ef9cfe436ff768030616
Arben Ferati <arben@ferati.org>
2022-05-04 15:51:07 +0200

    (views)ajout de la section role et permission pour les administrateurs

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -43,6 +43,75 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">{{ __('Roles & permissions') }}</div>
+
+                <div class="card-body">
+
+                    <h4>Roles</h4>
+
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th style="text-align: right">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">Vendeur</td>
+                                <td style="text-align: right">
+                                    <a name="confirm-btn" id="confirm-btn" class="btn btn-primary" href="" role="button">Modifier</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Admin</td>
+                                <td style="text-align: right">
+                                    <a name="confirm-btn" id="confirm-btn" class="btn btn-primary" href="" role="button">Modifier</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="d-grid gap-2">
+                      <button type="button" name="add-role-btn" id="add-role-btn" class="btn btn-primary">Ajouter role</button>
+                    </div>
+
+                    <hr>
+
+                    <h4>Permissions</h4>
+
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th style="text-align: right">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">Ajouter livreur</td>
+                                <td style="text-align: right">
+                                    <a name="confirm-btn" id="confirm-btn" class="btn btn-primary" href="" role="button">Modifier</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Engager livreur</td>
+                                <td style="text-align: right">
+                                    <a name="confirm-btn" id="confirm-btn" class="btn btn-primary" href="" role="button">Modifier</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="d-grid gap-2">
+                      <button type="button" name="add-role-btn" id="add-role-btn" class="btn btn-primary">Ajouter permission</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
