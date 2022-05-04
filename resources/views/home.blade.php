f7ce14d5a3302bd70a03b7025f796ed0db62e600
Arben Ferati <arben@ferati.org>
2022-05-04 15:08:14 +0200

    (views)ajout de table pour les vendeurs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,20 +7,36 @@
 @endguest
 
 @auth
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Commandes en cours') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nom</th>
+                                <th>Status</th>
+                                <th style="text-align: right">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">100</td>
+                                <td>SmartTV Samsung SERIE9</td>
+                                <td><span class="badge rounded-pill bg-warning">En attente de livraison</span></td>
+                                <td style="text-align: right">
+                                    <a name="edit-btn" id="edit-btn" class="btn btn-primary" href="" role="button">Edit</a>
+                                    <a name="delete-btn" id="delete-btn" class="btn btn-danger" href="" role="button">Delete</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
