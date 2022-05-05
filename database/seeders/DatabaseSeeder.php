fe3e8d398078e8a481e5c561000947b8ebfe49b0
Arben Ferati <arben@ferati.org>
2022-05-05 08:20:35 +0200

    (database)creation de quelques user avec différents roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\Store;
+use \App\Models\User;
+
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +21,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Administrateur',
+            'email' => 'admin@qd.com',
+            'password' => Hash::make('password')
+        ]);
+
+        User::factory(5)->create();
+        
+        Role::create([
+            'name' => 'admin'
+        ]);
+
+        Role::create([
+            'name' => 'vendeur'
+        ]);
+
+        
+        DB::table('user_has_role')->insert([
+            'user_id' => 1,
+            'role_id' => 1,
+        ]);
+
+        for ($i=2; $i <= 6 ; $i++) { 
+            Store::create([
+                'name' => Str::random(10),
+                'phone' => '+41 24 000 19 9' . $i,
+                'address' => Str::random(13),
+                'zip' => '180' . $i,
+                'city' => Str::random(6),
+                'user_id' => $i,
+            ]);
+
+            DB::table('user_has_role')->insert([
+                'user_id' => $i,
+                'role_id' => 2,
+            ]);
+        }
+
     }
 }
