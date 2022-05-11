90e0a1938592cc4dd7e6a29b665b208b96cd1f7f
Arben Ferati <arben@ferati.org>
2022-05-11 14:53:13 +0200

    (routes)ajouter les routes en relation avec les produits

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Sellers\ProductController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -23,3 +24,12 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/delete-user-{id}', [UserController::class, 'deleteUser'])->name('delete-user');
 Route::get('/enable-user-{id}', [UserController::class, 'enableUser'])->name('enable-user');
 Route::get('/disable-user-{id}', [UserController::class, 'disableUser'])->name('disable-user');
+
+
+Route::middleware('isSeller')->group(function(){
+    Route::post('/add-product', [ProductController::class, 'addProduct'])->name('add-product');
+    Route::post('/edit-product-{id}', [ProductController::class, 'editProduct'])->name('edit-product');
+    Route::get('/delete-product-{id}', [ProductController::class, 'deleteProduct'])->name('delete-product');
+    Route::get('/restore-product-{id}', [ProductController::class, 'restoreProduct'])->name('restore-product');
+    Route::get('/deleted-products', [ProductController::class, 'deletedProducts'])->name('deleted-products');
+});
