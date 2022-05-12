5a52a7fd47303cddd1ec53cd8543311c7ea68f8f
Arben Ferati <arben@ferati.org>
2022-05-12 08:42:40 +0200

    (models)ajout de la class Order avec les methodes getOrder, attachProductToOrder, createOrder, et products

---
+++ app/Models/Order.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'store_id',
+    ];
+
+
+    public function getOrder(int $id)
+    {
+        return $this::findOrFail($id);
+    }
+
+    public function attachProductToOrder(Order $order, Product $product)
+    {
+        return DB::table('order_has_product')->insert(['product_id' => $product->id, 'order_id' => $order->id]);
+    }
+
+    public function createOrder(array $products, int $customerId, int $storeId)
+    {
+        $order = $this::create(['customer_id' => $customerId, 'store_id' => $storeId]);
+
+        foreach ($products as $product) {
+            $result = $this->attachProductToOrder($order, $product);
+        }
+
+        return $order;
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_has_product');
+    }
+
+}
