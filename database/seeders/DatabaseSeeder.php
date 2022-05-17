36f9e94ad36d2228b6cc2b8c1ac2f9ff40a462ab
Arben Ferati <arben@ferati.org>
2022-05-17 10:42:06 +0200

    (database)seeder refait avec ajout de livreurs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Deliverer;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\Role;
@@ -10,9 +11,6 @@ use \App\Models\User;
 
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,88 +21,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Administrateur',
-            'email' => 'admin@qd.com',
-            'password' => Hash::make('password')
-        ]);
-
-        $user = User::create([
-            'name' => 'Sam Sepiol',
-            'email' => 'ss@qd.com',
-            'password' => Hash::make('password')
-        ]);
-
-        $user->validated_at = now();
-        $user->save();
-
-        $user = null;
-
-        User::create([
-            'name' => 'Client',
-            'email' => 'c@qd.com',
-            'password' => Hash::make('password')
-        ]);
-
-        User::factory(2)->create();
-
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'vendeur']);
-        Role::create(['name' => 'client']);
-
-
-        DB::table('user_has_role')->insert([
-            'user_id' => 1,
-            'role_id' => 1,
-        ]);
-
-        DB::table('user_has_role')->insert([
-            'user_id' => 2,
-            'role_id' => 2,
-        ]);
-
-        DB::table('user_has_role')->insert([
-            'user_id' => 3,
-            'role_id' => 3,
-        ]);
-
-        $store = Store::create([
-            'name' => 'Mr. Robot store',
-            'phone' => '+41 24 000 00 00',
-            'address' => 'Rue qwerty 3',
-            'zip' => '1050',
-            'city' => 'NY',
-            'user_id' => 2,
-        ]);
-
-        $productData = array();
-
-        for ($i=1; $i < 11; $i++) {
-            $product = Product::create(['name' => 'Produit N°' . $i, 'store_id' => $store->id]);
-            array_push($productData, $product);
-        }
-
+        $role = new Role();
+        $user = new User();
+        $store = new Store();
         $order = new Order();
-        $order = $order->createOrder($productData, 3, $store->id);
+        $product = new Product();
+        $deliverer = new Deliverer();
 
+        /** Création de l'utilisateur admin avec le role admin */
+            $user = $user->createUser(['name' => 'Administrateur', 'email' => 'admin@qd.com', 'password' => 'password']);
+            $role = $role->createRole(['name' => 'admin']);
+            $role->assignToUser($user);
+        /** Fin création de l'utilisateur admin */
 
-        for ($i=4; $i <= 5 ; $i++) {
-            $store = Store::create([
-                'name' => Str::random(10),
-                'phone' => '+41 24 000 19 9' . $i,
-                'address' => Str::random(13),
-                'zip' => '180' . $i,
-                'city' => Str::random(6),
-                'user_id' => $i,
-            ]);
+        /** Création de l'utilisateur vendeur avec le role vendeur ainsi que son magasin */
+            $user = $user->createUser(['name' => 'Sam Sepiol', 'email' => 'ss@qd.com', 'password' => 'password']);
+            $user->validateUser();
+            $role = $role->createRole(['name' => 'vendeur']);
+            $role->assignToUser($user);
+            $store = $store->createStore(['store-name' => 'Mr. Robot store', 'phone' => '+41 24 000 00 00', 'address' => 'Rue qwerty 3', 'zip' => '1050', 'city' => 'NY', 'user_id' => $user->id], $user->id);
+        /** Fin création de l'utilisateur vendeur avec le role vendeur  */
 
-            for ($j=4; $j <= 5; $j++) {
-                Product::create(['name' => 'Produit N°' . $j - 3, 'store_id' => $store->id]);
+        /** Création de produits */
+            $productData = array();
+            for ($i=1; $i <= 5; $i++) {
+                $product = $product->createProduct(['name' => 'Produit N°' . $i, 'store_id' => $store->id]);
+                array_push($productData, $product);
             }
+        /** Fin création de produits */
 
-            DB::table('user_has_role')->insert([ 'user_id' => $i, 'role_id' => 2, ]);
+        /** Création de l'utilisateur client avec le role client et une commande*/
+            $role = $role->createRole(['name' => 'client']);
+            $user = $user->createCustomer(['name' => 'Client', 'email' => 'c@qd.com', 'password' => 'password']);
+            $order = $order->createOrder($productData, $user->id, $store->id);
+        /** Fin création de l'utilisateur client avec le role client */
 
-        }
-
+        /** Création livreurs et le role livreur */
+            $role = $role->createRole(['name' => 'livreur']);
+            $deliverer = $deliverer->createDeliverer(['name' => 'Dwight Schrut', 'email' => 'ds@qd.com', 'password' => 'password'], $store);
+            $deliverer = $deliverer->createDeliverer(['name' => 'Jim Helpert', 'email' => 'jh@qd.com', 'password' => 'password'], $store);
+            $deliverer = $deliverer->createDeliverer(['name' => 'Stanley Hudson', 'email' => 'sh@qd.com', 'password' => 'password']);
+            $deliverer->askCollab($store);
+        /** Fin création livreurs et le role livreur */
     }
 }
