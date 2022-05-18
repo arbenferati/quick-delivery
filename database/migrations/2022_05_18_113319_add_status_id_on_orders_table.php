7f62fd7d99cbeae18755d44e855b83ae0a84346b
Arben Ferati <arben@ferati.org>
2022-05-18 16:27:34 +0200

    (database)ajout de la table order_status et la colone order_status_id dans la table orders

---
+++ database/migrations/2022_05_18_113319_add_status_id_on_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('order_status_id')
+                    ->after('store_id')
+                    ->nullable()
+                    ->references('id')
+                    ->on('order_status')
+                    ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('order_status_id');
+        });
+    }
+};
