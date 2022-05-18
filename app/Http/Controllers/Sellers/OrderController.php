d017a5005d873e109cca8b84a18e02b853044fa9
Arben Ferati <arben@ferati.org>
2022-05-18 16:31:43 +0200

    (controllers)ajout des methode cancelOrder et updateStatus

--- app/Http/Controllers/Sellers/OrderController.php
+++ app/Http/Controllers/Sellers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Sellers;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class OrderController extends Controller
 {
@@ -14,5 +15,24 @@ class OrderController extends Controller
         $order = $order->getOrder($id);
 
         return view('orders.show', compact('order'));
+    }
+
+    public function cancelOrder($id)
+    {
+        $order = new Order();
+        $order = $order->getOrder($id);
+
+        $order->updateStatus(4);
+
+        return Redirect()->home()->with('success', 'La commande à bien été annulée');
+    }
+
+    public function updateStatus($order_id, $status_id)
+    {
+        $order = new Order();
+        $order = $order->getOrder($order_id);
+        $order->updateStatus($status_id);
+
+        return Redirect()->home()->with('success', 'Le statut de la commande à été mis à jour');
     }
 }
