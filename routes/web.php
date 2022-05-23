90429c70f028cdf785339c97616816dd003b2fac
Arben Ferati <arben@ferati.org>
2022-05-23 07:54:30 +0200

    (routes)ajouter la route 'update-status' avec la methode POST

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::middleware('isSeller')->group(function(){
     Route::get('/order-{id}', [OrderController::class, 'show'])->name('show-order');
     Route::get('/cancel-order-{id}', [OrderController::class, 'cancelOrder'])->name('cancel-order');
     Route::get('/update-order-{order_id}-status-{status_id}', [OrderController::class, 'updateStatus'])->name('update-order-status');
+    Route::post('/update-status', [OrderController::class, 'update'])->name('update-status');
 
     Route::get('/deliverers', [DelivererController::class, 'index'])->name(('index-deliverers'));
     //Route::get('/cancel-request-deliverer-{id}', [DelivererController::class, 'cancelRequestDeliverer'])->name('cancel-deliverer-request');
